17c86bce787e615690e5884678748767bee88d2b
Towshin <99317247+TS-Towshin@users.noreply.github.com>
2023-09-07 21:15:41 +0600

    Add files via upload

---
+++ anyBaseToDecimal.py
@@ -0,0 +1,15 @@
+# This script can only convert from base up to 36
+
+def convert(N, b):
+    dec_val = 0
+    N = str(N)
+    indx = len(N)
+    for i in range(indx):
+        indx -= 1
+        try:
+            dec_val += int(N[indx])*(b**i)
+        except:
+            code = {'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15, 'G':16, 'H':17, 'I':18, 'J':19, 'K':20, 'L':21, 'M':22, 'N':23, 'O':24, 'P':25, 'Q':26, 'R':27, 'S':28, 'T':29, 'U':30, 'V':31, 'W':32, 'X':33, 'Y':34, 'Z':35}
+
+            dec_val = dec_val + code[N[indx]]*(b**i)
+    print(dec_val)
---
+++ main.py
@@ -0,0 +1,24 @@
+import anyBaseToDecimal
+import decimalToAnyBase
+
+print("'exit' to quit")
+while True:
+    print("[1] Decimal to another base")
+    print("[2] Any base to decimal")
+
+    choice = input(">>> ")
+    if choice == '1':
+        decimalToAnyBase
+        N = int(input("Decimal number: "))
+        b = int(input("Enter the base: "))
+        decimalToAnyBase.convert(N, b)
+
+    elif choice == '2':
+        anyBaseToDecimal
+        N = input("Enter the value: ")
+        b = int(input("Enter the base: "))
+        anyBaseToDecimal.convert(N, b)
+    elif choice == 'exit':
+        exit()
+    else:
+        print("please enter a valid option")
